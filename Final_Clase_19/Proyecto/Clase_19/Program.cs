5309273079e5937063a4b0f926e7797133f727a5
UTN-FRA. Tecnicaturas en programación <tecnicaturasutnfra@gmail.com>
2016-09-09 16:10:22 -0300

    Add files via upload

---
+++ Final_Clase_19/Proyecto/Clase_19/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clase_8_Library;
+using Archivos;
+
+namespace Clase_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado e1 = new Empleado("José", "Pérez", "000-001", Empleado.EPuestoJerarquico.Gerencia, 45000);
+
+            Console.WriteLine("XML");
+            Xml<Empleado> xml = new Xml<Empleado>();
+            if (!xml.guardar("Empleado.xml", e1))
+                Console.WriteLine("Error al guardar XML");
+
+            Empleado e2;
+            if (!xml.leer("Empleado.xml", out e2))
+                Console.WriteLine("Error al leer XML");
+            else
+                Console.WriteLine(e2.ToString());
+
+            Console.WriteLine("<----------------->");
+            Console.ReadKey();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("TXT");
+            Texto txt = new Texto();
+            if (!txt.guardar("Empleado.txt", e1.ToString()))
+                Console.WriteLine("Error al guardar TXT");
+
+            string datos;
+            if (!txt.leer("Empleado.txt", out datos))
+                Console.WriteLine("Error al leer TXT");
+            else
+                Console.WriteLine(datos);
+
+            Console.WriteLine("<----------------->");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Final_Clase_19/Proyecto/Clase_19_Archivos/Binario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Binario : IArchivo<string>
+    {
+        public bool guardar(string archivo, string datos)
+        {
+            throw new NotImplementedException("Método no implementado");
+        }
+        public bool leer(string archivo, out string datos)
+        {
+            throw new NotImplementedException("Método no implementado");
+        }
+    }
+}
