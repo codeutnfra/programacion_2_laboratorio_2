2414117679837807543aebca0146189ed8e69cd0
UTN-FRA. Tecnicaturas en programación <tecnicaturasutnfra@gmail.com>
2016-09-09 16:07:21 -0300

    Add files via upload

---
+++ Final_Clase_18/Proyecto/Clase_18_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_18_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vista vista = new Vista();
+            vista.Main(args);
+        }
+    }
+}
---
+++ Final_Clase_18/Proyecto/Clase_18_WindowsForms/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Clase_8_Library;
+using Clase_18_Library;
+
+namespace Clase_18_WindowsForms
+{
+    public partial class frmPrueba : Form, IGrafica
+    {
+        public frmPrueba()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrueba_Load(object sender, EventArgs e)
+        {
+            Empleado empleado = new Empleado("Jorge", "Martínez", "123-456", Empleado.EPuestoJerarquico.Sistemas, 15000);
+            empleado.Mostrar(this);
+        }
+
+        #region IGrafica Members
+
+        public void MostrarEnGUI(string datos)
+        {
+            lblTexto.Text = datos;
+        }
+
+        #endregion
+    }
+}
