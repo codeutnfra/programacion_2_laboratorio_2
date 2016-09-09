4b749c869e2818b82d48caeb5ee94e8998ccef3c
UTN-FRA. Tecnicaturas en programación <tecnicaturasutnfra@gmail.com>
2016-09-09 15:47:31 -0300

    Add files via upload

---
+++ Final_Clase_11/Proyecto/Clase_11/frmEmpresa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Clase_8_Library;
+
+namespace Clase_8
+{
+    public partial class frmEmpresa : Form
+    {
+        Empresa _empresa;
+
+        public Empresa Empresa
+        {
+            get
+            {
+                return this._empresa;
+            }
+        }
+
+        public frmEmpresa(Empresa empresa)
+        {
+            InitializeComponent();
+
+            this._empresa = empresa;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            float ganancias = float.Parse(this.mtxtGanancias.Text.Replace(".",",").Substring(1, mtxtGanancias.Text.Length - 1));
+            if (this._empresa == null)
+            {
+                this._empresa = new Empresa(this.txtRazonSocial.Text, this.txtDireccion.Text, ganancias);
+            }
+            else
+            {
+                this._empresa.RazonSocial = this.txtRazonSocial.Text;
+                this._empresa.Direccion = this.txtDireccion.Text;
+                this._empresa.Ganancias = ganancias;
+            }
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Close();
+        }
+    }
+}
---
+++ Final_Clase_11/Proyecto/Clase_11_Library/Empleado.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_Library
+{
+    public class Empleado : Persona
+    {
+        public enum EPuestoJerarquico
+        {
+            Administración,
+            Gerencia,
+            Sistemas,
+            Accionista
+        }
+        string _legajo;
+        EPuestoJerarquico _puesto;
+        int _salario;
+
+        public Empleado(Persona persona, string legajo, EPuestoJerarquico puesto, int salario)
+            : this(persona.Nombre, persona.Apellido, legajo, puesto, salario)
+        {
+        }
+
+        public Empleado(string nombre, string apellido, string legajo, EPuestoJerarquico puesto, int salario)
+            : base(nombre, apellido)
+        {
+            this._legajo = legajo;
+            this._puesto = puesto;
+            this._salario = salario;
+        }
+
+        public override string PosicionSocietaria()
+        {
+            return "Empleado de " + this._puesto.ToString();
+        }
+
+        /// <summary>
+        /// Dos empleados son iguales si, y sólo si, comparten el mismo número de legajo.
+        /// </summary>
+        /// <param name="e1">Primer empleado a comparar</param>
+        /// <param name="e2">Segundo empleado a comparar</param>
+        /// <returns></returns>
+        public static bool operator ==(Empleado e1, Empleado e2)
+        {
+            // Controlo que ninguno de los dos empleados no haya sido instanciado, para evitar errores.
+            if (!object.ReferenceEquals(e1, null) && !object.ReferenceEquals(e2, null))
+            {
+                if (e1._legajo == e2._legajo)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Dos empleados son distintos si, y sólo si, NO comparten el mismo número de legajo.
+        /// </summary>
+        /// <param name="e1">Primer empleado a comparar</param>
+        /// <param name="e2">Segundo empleado a comparar</param>
+        /// <returns></returns>
+        public static bool operator !=(Empleado e1, Empleado e2)
+        {
+            return !(e1 == e2);
+        }
+
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Persona.Mostrar(this));
+            sb.AppendLine("Legajo  : " + this._legajo);
+            sb.AppendLine(this.PosicionSocietaria()); //"Puesto  : " + this._puesto.ToString());
+            sb.AppendLine("Salario : $" + this._salario);
+            sb.AppendLine("******************");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+    }
+}
