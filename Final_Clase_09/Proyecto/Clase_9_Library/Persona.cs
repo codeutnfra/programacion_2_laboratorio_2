a383ff480d53e8594970641d931f18fb2370f735
UTN-FRA. Tecnicaturas en programación <tecnicaturasutnfra@gmail.com>
2016-09-09 15:43:53 -0300

    Add files via upload

---
+++ Final_Clase_09/Proyecto/Clase_9_Library/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_Library
+{
+    public class Persona
+    {
+        protected string _nombre;
+        protected string _apellido;
+
+        public Persona(string nombre, string apellido)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+        }
+        public string Apellido
+        {
+            get
+            {
+                return this._apellido;
+            }
+        }
+    }
+}
