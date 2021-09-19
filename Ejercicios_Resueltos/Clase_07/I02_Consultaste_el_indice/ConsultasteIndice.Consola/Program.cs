4e111b5e379ca234df62730334c0b1b749a546a3
mauriciocerizza <mauriciocerizza@gmail.com>
2021-09-19 18:19:24 -0300

    Ejercicio Clase 07 - I02

---
+++ Ejercicios_Resueltos/Clase_07/I02_Consultaste_el_indice/ConsultasteIndice.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsultasteIndice.Entidades;
+
+namespace ConsultasteIndice.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+            libro[0] = "Jon I";
+            libro[1] = "Daenerys II";
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(libro[i]);
+            }
+            libro[1] = "Tyrion III";
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(libro[i]);
+            }
+        }
+    }
+}
---
+++ Ejercicios_Resueltos/Clase_07/I02_Consultaste_el_indice/ConsultasteIndice.Entidades/Libro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsultasteIndice.Entidades
+{
+    public class Libro
+    {
+        private List<string> paginas;
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < this.paginas.Count)
+                {
+                    return paginas[i];
+                }
+                return "Pagina no valida";
+            }
+
+            set
+            {
+                if (i > this.paginas.Count)
+                {
+                    this.paginas.Add(value);
+                }
+                else if (i >= 0)
+                {
+                    this.paginas.Insert(i, value);
+                }
+            }
+        }
+    }
+}
