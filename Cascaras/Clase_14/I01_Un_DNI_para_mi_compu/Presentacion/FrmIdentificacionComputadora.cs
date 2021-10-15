cfa98b03bf1f054974b2126f8d4b69fc960b13b8
mauriciocerizza <mauriciocerizza@gmail.com>
2021-10-15 15:43:04 -0300

    cascaras

---
+++ Cascaras/Clase_14/I01_Un_DNI_para_mi_compu/Presentacion/FrmIdentificacionComputadora.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class FrmIdentificacionComputadora : Form
+    {
+        public FrmIdentificacionComputadora()
+        {
+            InitializeComponent();
+        }
+    }
+}
